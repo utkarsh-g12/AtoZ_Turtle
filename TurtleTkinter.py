40407fdafaddc96936ee747e88a2e91144f691b4
ut-available-here <138252781+ut-available-here@users.noreply.github.com>
2023-07-15 19:35:34 +0530

    Add files via upload

---
+++ TurtleTkinter.py
@@ -0,0 +1,555 @@
+from tkinter import messagebox
+from tkinter import *
+
+root = Tk()
+root.title("Alphabet Shapes")
+root.geometry("600x500")
+try:
+
+
+
+    def test():
+        from turtle import Turtle, Screen, done
+        Screen().clear()
+        Screen().bgcolor("Red")
+        t = Turtle()
+        t.hideturtle()
+        t.write("The Game is Over", align="center", move=False)
+        done()
+
+
+    def ShapeA():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.up()
+        t.back(50)
+        t.down()
+        t.left(55)
+        t.forward(250)
+        t.right(115)
+        t.forward(250)
+        t.up()
+        t.back(125)
+        t.down()
+        t.right(120)
+        t.forward(140)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeB():
+        from turtle import Turtle, Screen,done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(90)
+        t.forward(200)
+        t.left(-270)
+        t.circle(50, extent=-180)
+        t.right(180)
+        t.circle(50, extent=-180)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeC():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(180)
+        t.circle(100, extent=180)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeD():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(90)
+        t.forward(200)
+        t.left(90)
+        t.circle(100, extent=-180)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeE():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(90)
+        t.forward(100)
+        t.back(200)
+        t.right(90)
+        t.forward(100)
+        t.back(100)
+        t.left(90)
+        t.forward(100)
+        t.right(90)
+        t.forward(100)
+        t.back(100)
+        t.left(90)
+        t.forward(100)
+        t.right(90)
+        t.forward(100)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeF():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(90)
+        t.forward(100)
+        t.right(90)
+        t.forward(100)
+        t.back(100)
+        t.left(90)
+        t.forward(100)
+        t.right(90)
+        t.forward(100)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeG():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.forward(80)
+        t.right(90)
+        t.forward(10)
+        t.right(-180)
+        t.circle(150, extent=-300)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeH():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(90)
+        t.forward(200)
+        t.back(100)
+        t.right(90)
+        t.forward(100)
+        t.left(90)
+        t.forward(100)
+        t.back(200)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeI():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(90)
+        t.forward(200)
+        t.right(90)
+        t.forward(50)
+        t.back(100)
+        t.forward(50)
+        t.right(90)
+        t.forward(200)
+        t.right(90)
+        t.forward(50)
+        t.back(100)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeX():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(45)
+        t.forward(200)
+        t.up()
+        t.right(45)
+        t.back(150)
+        t.down()
+        t.right(45)
+        t.forward(200)
+        sleep(1)
+        test()
+        done()
+
+
+
+
+
+
+    def ShapeK():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(90)
+        t.forward(200)
+        t.back(100)
+        t.right(45)
+        t.forward(150)
+        t.back(150)
+        t.right(90)
+        t.forward(150)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeL():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(90)
+        t.forward(200)
+        t.back(200)
+        t.right(90)
+        t.forward(90)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeM():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(90)
+        t.forward(200)
+        t.right(165)
+        t.forward(200)
+        t.left(145)
+        t.forward(200)
+        t.right(160)
+        t.forward(200)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeN():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(90)
+        t.forward(200)
+        t.right(145)
+        t.forward(250)
+        t.left(145)
+        t.forward(200)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeO():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.circle(100)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeP():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(90)
+        t.forward(200)
+        t.right(-90)
+        t.circle(50, extent=-180)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeQ():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.circle(100)
+        t.up()
+        t.right(-45)
+        t.forward(50)
+        t.down()
+        t.left(90)
+        t.back(150)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeR():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(90)
+        t.forward(200)
+        t.right(-90)
+        t.circle(50, extent=-180)
+        t.right(45)
+        t.forward(200)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeS():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.right(180)
+        t.circle(50, extent=180)
+        t.right(180)
+        t.circle(50, extent=-270)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeT():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(90)
+        t.forward(200)
+        t.right(90)
+        t.forward(50)
+        t.back(100)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeU():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(-90)
+        t.forward(100)
+        t.right(180)
+        t.circle(50, extent=-180)
+        t.left(180)
+        t.forward(100)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeV():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.up()
+        t.back(50)
+        t.down()
+        t.right(55)
+        t.forward(250)
+        t.left(115)
+        t.forward(250)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeW():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(90)
+        t.forward(200)
+        t.back(200)
+        t.right(25)
+        t.forward(200)
+        t.right(140)
+        t.forward(200)
+        t.left(165)
+        t.forward(220)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeJ():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.left(90)
+        t.forward(200)
+        t.left(90)
+        t.forward(50)
+        t.back(50)
+        t.right(-90)
+        t.forward(200)
+        t.left(-270)
+        t.circle(90, extent=-90)
+        sleep(1)
+        test()
+        done()
+
+
+
+    def ShapeY():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.up()
+        t.left(90)
+        t.forward(50)
+
+        t.down()
+        t.left(45)
+        t.forward(200)
+        t.back(200)
+        t.right(90)
+        t.forward(200)
+        t.back(200)
+        t.right(135)
+        t.forward(200)
+        sleep(1)
+        test()
+        done()
+
+
+    def ShapeZ():
+        from turtle import Turtle, Screen, done
+        from time import sleep
+        Screen().clear()
+        t = Turtle()
+        t.hideturtle()
+        t.forward(100)
+        t.right(115)
+        t.forward(200)
+        t.left(115)
+        t.forward(100)
+        sleep(1)
+        test()
+
+        done()
+
+
+    btnA = Button(root, text="A", font=("Ariel", 30, "bold"), command=ShapeA)
+    btnA.grid(row=0, column=0)
+    btnB = Button(root, text="B", font=("Ariel", 30, "bold"), command=ShapeB)
+    btnB.grid(row=0, column=1)
+    btnC = Button(root, text="C", font=("Ariel", 30, "bold"), command=ShapeC)
+    btnC.grid(row=0, column=2)
+    btnD = Button(root, text="D", font=("Ariel", 30, "bold"), command=ShapeD)
+    btnD.grid(row=0, column=3)
+    btnE = Button(root, text="E", font=("Ariel", 30, "bold"), command=ShapeE)
+    btnE.grid(row=0, column=4)
+    btnF = Button(root, text="F", font=("Ariel", 30, "bold"), command=ShapeF)
+    btnF.grid(row=0, column=5)
+    btnG = Button(root, text="G", font=("Ariel", 30, "bold"), command=ShapeG)
+    btnG.grid(row=0, column=6)
+    btnH = Button(root, text="H", font=("Ariel", 30, "bold"), command=ShapeH)
+    btnH.grid(row=0, column=7)
+    btnI = Button(root, text="I", font=("Ariel", 30, "bold"), command=ShapeI)
+    btnI.grid(row=1, column=0)
+    btnJ = Button(root, text="J", font=("Ariel", 30, "bold"), command=ShapeJ)
+    btnJ.grid(row=1, column=1)
+    btnK = Button(root, text="K", font=("Ariel", 30, "bold"), command=ShapeK)
+    btnK.grid(row=1, column=2)
+    btnL = Button(root, text="L", font=("Ariel", 30, "bold"), command=ShapeL)
+    btnL.grid(row=1, column=3)
+    btnM = Button(root, text="M", font=("Ariel", 30, "bold"), command=ShapeM)
+    btnM.grid(row=1, column=4)
+    btnN = Button(root, text="N", font=("Ariel", 30, "bold"), command=ShapeN)
+    btnN.grid(row=1, column=5)
+    btnO = Button(root, text="O", font=("Ariel", 30, "bold"), command=ShapeO)
+    btnO.grid(row=1, column=6)
+    btnP = Button(root, text="P", font=("Ariel", 30, "bold"), command=ShapeP)
+    btnP.grid(row=1, column=7)
+    btnQ = Button(root, text="Q", font=("Ariel", 30, "bold"), command=ShapeQ)
+    btnQ.grid(row=2, column=0)
+    btnR = Button(root, text="R", font=("Ariel", 30, "bold"), command=ShapeR)
+    btnR.grid(row=2, column=1)
+    btnS = Button(root, text="S", font=("Ariel", 30, "bold"), command=ShapeS)
+    btnS.grid(row=2, column=2)
+    btnT = Button(root, text="T", font=("Ariel", 30, "bold"), command=ShapeT)
+    btnT.grid(row=2, column=3)
+    btnU = Button(root, text="U", font=("Ariel", 30, "bold"), command=ShapeU)
+    btnU.grid(row=2, column=4)
+    btnV = Button(root, text="V", font=("Ariel", 30, "bold"), command=ShapeV)
+    btnV.grid(row=2, column=5)
+    btnW = Button(root, text="W", font=("Ariel", 30, "bold"), command=ShapeW)
+    btnW.grid(row=2, column=6)
+    btnX = Button(root, text="X", font=("Ariel", 30, "bold"), command=ShapeX)
+    btnX.grid(row=2, column=7)
+    btnY = Button(root, text="Y", font=("Ariel", 30, "bold"), command=ShapeY)
+    btnY.grid(row=3, column=0)
+    btnZ = Button(root, text="Z", font=("Ariel", 30, "bold"), command=ShapeZ)
+    btnZ.grid(row=3, column=1)
+
+
+
+
+except Exception:
+    messagebox.showerror("TurtleTkinter","Something went wrong")
+
+mainloop()
